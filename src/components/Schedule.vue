7c126d354f99a8bfabbc10a77979efab94443e89
“Eric <eands9@yahoo.com>
2019-09-10 06:32:54 -0500

    added schedule

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,13 +1,24 @@
 <template>
-  <v-container>
-    <v-layout text-center>
-      <v-flex mb-4>
-        <v-card-text>
-          <p>
-            This is my Schedule Page
-          </p>
-        </v-card-text>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div class=team>
+    <h1 class="mx-4 mb-4 subheading">Schedule</h1>
+      <v-container class=mx-5 mb-4 my-5>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime et earum tempore quibusdam. Architecto in accusamus beatae autem repellat cum suscipit eaque sed aspernatur, molestiae, culpa dolores, repudiandae libero eius.</p>
+        <v-card class="flat pa-3">
+          <v-layout row wrap>
+            <v-flex class="xs12 md6">
+              <div class="caption">Tournament</div>
+              <div>South X SouthWest</div>
+            </v-flex>
+            <v-flex class="xs6 xm4 md2">
+              <div class="caption">Location</div>
+              <div>Waters Park</div>
+            </v-flex>
+            <v-flex class="xs6 xm4 md2">
+              <div class="caption">Date</div>
+              <div>September 14, 2019</div>
+            </v-flex>
+          </v-layout>
+        </v-card> 
+      </v-container>
+  </div>
 </template>
