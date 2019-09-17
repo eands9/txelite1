9120c1515a3bb5c8e36fc4cf8066beebb77503e1
“Eric <eands9@yahoo.com>
2019-09-16 20:39:56 -0500

    before adding carousel

--- src/views/Roster.vue
+++ src/views/Roster.vue
@@ -6,22 +6,22 @@
 
       <v-layout row wrap>
         <v-flex xs12 sm6 md4 lg3 v-for="player in team" :key="player.name">
-          <v-card class="text-xs-center ma-3">
-            <v-responsive class="pt-4">
-              <v-avatar size="100" class="grey lighten-2">
-                <img :src="player.avatar">
-              </v-avatar>
-            </v-responsive>
-            <v-card-text center>
-              <div class="black--text">{{ player.name }}</div>
-              <div>{{ player.position }}</div>
+          <v-card max-width=350 class="black ma-3">
+            <v-row justify="space-around">
+              <v-responsive class="pt-4">
+                <v-avatar size="100" class="grey lighten-2">
+                  <img :src="player.avatar">
+                </v-avatar>
+              </v-responsive>
+            </v-row>
+            <v-card-text class="white--text text-center">
+              <h4>{{ player.name }}</h4>
+              <h5>{{ player.position }}</h5>
             </v-card-text>
           </v-card>
         </v-flex>
       </v-layout>
-
     </v-container>
-    
   </div>
 </template>
 
@@ -30,11 +30,18 @@ export default {
   data() {
     return {
       team: [
-        { name: '#1 Kate Hernandez', position: 'RHP/SS', avatar: '/avatar-1.png' },
-        { name: '#3 Emma Alex', position: '2B/OF', avatar: '/avatar-2.png' },
+        { name: '#27 Addison Pecenka', position: '3B/OF', avatar: '/avatar-3.png' },
+        { name: '#23 Alyria Smith', position: '3B/OF', avatar: '/avatar-3.png' },
+        { name: '#48 Amelia Nell', position: '2B/OF', avatar: '/avatar-3.png' },
+        { name: '#6 Bellalucia Cantu', position: 'LHP/1B', avatar: '/avatar-3.png'},
         { name: '#4 Drew Amarro', position: 'C/SS', avatar: '/avatar-3.png' },
-        { name: '#5 Macie Bryant', position: 'RHP/3B', avatar: '/avatar-4.png' },
-        { name: '#6 Bellalucia Cantu', position: 'LHP/1B', avatar: '/avatar-5.png'}
+        { name: '#3 Emma Alex', position: '2B/OF', avatar: '/avatar-3.png' },
+        { name: '#2 Julia Wilson', position: '1B/OF', avatar: '/avatar-3.png' },
+        { name: '#1 Kate Hernandez', position: 'RHP/SS', avatar: '/avatar-3.png' },
+        { name: '#5 Macie Bryant', position: 'RHP/3B', avatar: '/avatar-3.png' },
+        { name: '#35 Maggie Blankenship', position: 'OF', avatar: '/avatar-3.png' },
+        { name: '#21 Whitney Flanigan', position: 'C/OF', avatar: '/avatar-3.png' },
+        
 
       ]
     }
--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -2,7 +2,7 @@
   <div class=team>
     <h1 class="mx-4 mb-4 subheading">Schedule</h1>
       <v-container class=mx-5 mb-4 my-5>
-        <v-card class="flat pa-3">
+        <v-card raised class="black white--text pa-3">
           <v-layout row wrap mx-2>
             <v-flex class="xs12 md6">
               <div class="caption">Tournament</div>
@@ -15,6 +15,22 @@
             <v-flex class="xs6 xm4 md2">
               <div class="caption">Date</div>
               <div>September 14, 2019</div>
+            </v-flex>
+          </v-layout>
+        </v-card> 
+        <v-card raised class="blue-grey darken-3 white--text pa-3">
+          <v-layout row wrap mx-2>
+            <v-flex class="xs12 md6">
+              <div class="caption">Practice</div>
+              <div>Team</div>
+            </v-flex>
+            <v-flex class="xs6 xm4 md2">
+              <div class="caption">Location</div>
+              <div>Jasper HS</div>
+            </v-flex>
+            <v-flex class="xs6 xm4 md2">
+              <div class="caption">Date</div>
+              <div>September 18, 2019</div>
             </v-flex>
           </v-layout>
         </v-card> 
