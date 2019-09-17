5445dafcba61a5c8dea53c5a2023ccea797dcef4
“Eric <eands9@yahoo.com>
2019-09-16 21:28:59 -0500

    add carousel pic deploy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,71 @@
 <template>
-  <div class=team>
-    <h1 class="mx-4 mb-4 subheading">Home</h1>
-      <v-container class=mx-4 mb-4 my-5>
-        <p>Under Construction</p>  
-      </v-container>
-  </div>
+  <v-container>
+    <div id="app">
+      <div id="ttt" class="container-fluid screen-2 panel" style="top: calc(1*100%);">
+        <div class="promo-slid">
+        </div>
+        <div class="slider">
+          <carousel-3d :autoplay="true" :controls-visible="true" :controls-prev-html="'&#10092;'" :controls-next-html="'&#10093;'" 
+                :controls-width="30" :controls-height="60" :clickable="false" :count="events.length">
+            <slide v-for="(event, i) in events" :index="i" :key="i">
+              <figure>
+                <img v-bind:src="event.images">
+              </figure>
+            </slide>
+          </carousel-3d>
+        </div>
+      </div>
+    </div>
+  </v-container>
 </template>
+
+<script>
+import { Carousel3d, Slide } from "vue-carousel-3d";
+
+export default {
+  name: "SomeName",
+  components: {
+    Carousel3d,
+    Slide
+  },
+
+  data() {
+    return {
+      events: []
+    };
+  }, // End of data
+
+  created() {
+    var vm = this;
+    setTimeout(function() {
+      vm.events = [
+        {
+          images: "https://txelite.s3.us-east-2.amazonaws.com/img/TeamWin.JPG"
+        },
+        {
+          images: "https://txelite.s3.us-east-2.amazonaws.com/img/JnK.jpeg"
+        },
+        {
+          images: "https://txelite.s3.us-east-2.amazonaws.com/img/ChadNBeetle.jpeg"
+        },
+        {
+          images: "https://txelite.s3.us-east-2.amazonaws.com/img/TeamWin2.jpeg"
+        },
+                {
+          images: "https://txelite.s3.us-east-2.amazonaws.com/img/KateNDrewNMacie.jpeg"
+        }
+      ];
+    }, 2000);
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 6px;
+}
+</style>
